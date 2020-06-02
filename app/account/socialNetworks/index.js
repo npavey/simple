eed6e3daea4ec411bc07ffa732c267b772182673
Alexandr Vanilchuk <60686870+ovanilchuk@users.noreply.github.com>
2020-06-02 09:41:26 +0300

    chore: additional query param (#588)

--- app/account/socialNetworks/index.js
+++ app/account/socialNetworks/index.js
@@ -4,8 +4,10 @@ define(function () {
     var _instance = null;
 
     var _getAuthLink = function (courseTitle, authServiceUrl, socialNetwork) {
+        const addTokenQueryParam =
+          window.location.host.indexOf('easygenerator.com') === -1 ? '&addTokenQuery=true' : '';
         return authServiceUrl + '/api/auth/' + socialNetwork +
-            '?callbackUrl=' + encodeURIComponent(window.location.href);
+            '?callbackUrl=' + encodeURIComponent(window.location.href) + addTokenQueryParam;
     };
 
     function SocialNetworks(courseTitle, authServiceUrl) {
