bc8037db94fdb07e75c8bc90865e021b6bcfacbb
Hlib Mykhailov <39822574+hlibmykhailov@users.noreply.github.com>
2021-01-26 21:37:35 +0200

    fix: xapi actor name (#600)
    Co-authored-by: Hlib Mykhailov <hlib@easygenerator.com>

--- app/xApi/models/actor.js
+++ app/xApi/models/actor.js
@@ -1,31 +1,37 @@
-﻿define(['../guard', './object'],
-    function (guard, object) {
-
-        var actor = function (spec) {
-
-            if (typeof spec == typeof undefined) {
-                throw 'You should provide a specification to create an Actor';
-            }
-
-            spec.objectType = 'Agent';
-            var obj = new object(spec);
-            
-            if(spec.account) {
-                guard.throwIfNotString(spec.account.homePage, 'You should provide homePage for Actor account');
-                guard.throwIfNotString(spec.account.name, 'You should provide name for Actor account');
-                obj.account = {
-                    homePage: spec.account.homePage,
-                    name: spec.account.name
-                };
-            } else {
-                guard.throwIfNotMbox(spec.mbox, 'You should provide mbox identity for Actor');
-                obj.mbox = spec.mbox;
-            }
-            obj.name = spec.name;
-
-            return obj;
-        };
-
-        return actor;
+﻿define(['../guard', './object'], function (guard, object) {
+  var actor = function (spec) {
+    if (typeof spec == typeof undefined) {
+      throw 'You should provide a specification to create an Actor'
     }
-);
+
+    spec.objectType = 'Agent'
+    var obj = new object(spec)
+
+    if (spec.account) {
+      guard.throwIfNotString(
+        spec.account.homePage,
+        'You should provide homePage for Actor account'
+      )
+      guard.throwIfNotString(
+        spec.account.name,
+        'You should provide name for Actor account'
+      )
+      obj.account = {
+        homePage: spec.account.homePage,
+        name: spec.account.name,
+      }
+      obj.name = spec.name || spec.account.name
+    } else {
+      guard.throwIfNotMbox(
+        spec.mbox,
+        'You should provide mbox identity for Actor'
+      )
+      obj.mbox = spec.mbox
+      obj.name = spec.name
+    }
+
+    return obj
+  }
+
+  return actor
+})
